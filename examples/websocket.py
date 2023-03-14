55ef39f9f437c7e036a83bfaffcf359bddab1a3f
Perry <57735040+perrys25@users.noreply.github.com>
2023-03-14 14:09:57 -0400

    Use websock instead of Websockets

--- examples/websocket.py
+++ examples/websocket.py
@@ -1,22 +1,15 @@
-import asyncio
 import json
 
 import openai
-import websockets
+from websock import *
 
 from SearchGPT.ChatBot import ChatBot
 from SearchGPT.Commands.SearchCommand import SearchCommand
 
+chatbots = {}
+nextID = 0
 
-async def handler(websocket, path):
-    progress = []
-
-    def sendProgress(p):
-        progress.append(p)
-
-    chatbot = ChatBot("SearchGPT",
-                      botType="Web UI based text-based search assist chatbot. The source code is available on https://github.com/perrys25/SearchGPT",
-                      progress=sendProgress, commands=[SearchCommand(
+search = SearchCommand(
             """
                     If a question is factual, or could benefit from the use of multiple internet searches, end the message with a "[Search]" followed by one or multiple related search terms to get information from the internet.
                     Always use a search function when getting data that could fluctuate from day to day, as your training data is from the past, and may be very out of date
@@ -26,32 +19,63 @@ async def handler(websocket, path):
                     This also means that you should only talk about relevant information, and not include any information that is not relevant to the question.
                     Since users will be viewing your responses on a website, you should also include links to the sources you used in your response, cited with markdown links in the format [1](https://example.com/testing), or [2](https://wikipedia.com/article)
                     """
-        )])
-    while True:
-        data = json.JSONDecoder().decode(await websocket.recv())
-        if data["type"] == "apikey":
-            chatbot.apiKey = data["apikey"]
-        if data["type"] == "question":
-            try:
-                reply = chatbot.ask(data["question"])
-                for p in progress:
-                    await websocket.send(json.JSONEncoder().encode(p))
-                progress.clear()
-                await websocket.send(json.JSONEncoder().encode({"type": "message", "message": reply}))
-            except openai.error.AuthenticationError:
-                await websocket.send(json.JSONEncoder().encode({"type": "error", "error": "Invalid API Key"}))
-            except openai.error.APIConnectionError:
-                await websocket.send(
-                    json.JSONEncoder().encode({"type": "error", "error": "Could not connect to OpenAI API"}))
-            except openai.error.APIError as e:
-                await websocket.send(
-                    json.JSONEncoder().encode({"type": "error", "error": "OpenAI API Error: " + str(e)}))
-            except Exception as e:
-                await websocket.send(json.JSONEncoder().encode({"type": "error", "error": "Unknown Error: " + str(e)}))
+        )
+
+def on_data_receive(client, stData):
+    global chatbots
+    data = json.loads(stData)
+    chatbot = chatbots[client]
+    if data["type"] == "apikey":
+        chatbot.apiKey = data["apikey"]
+        print("API Key set to " + chatbot.apiKey)
+    if data["type"] == "question":
+        try:
+            reply = chatbot.ask(data["question"])
+            server.send(client, json.JSONEncoder().encode({"type": "message", "message": reply}))
+        except openai.error.AuthenticationError:
+            server.send(client, json.JSONEncoder().encode({"type": "error", "error": "Invalid API Key"}))
+        except openai.error.APIConnectionError:
+            server.send(client,
+                json.JSONEncoder().encode({"type": "error", "error": "Could not connect to OpenAI API"}))
+        except openai.error.APIError as e:
+            server.send(client,
+                json.JSONEncoder().encode({"type": "error", "error": "OpenAI API Error: " + str(e)}))
+        except Exception as e:
+            server.send(client, json.JSONEncoder().encode({"type": "error", "error": "Unknown Error: " + str(e)}))
 
 
-start_server = websockets.serve(handler, "localhost", 8080)
 
-asyncio.get_event_loop().run_until_complete(start_server)
+def on_connection_open(client):
+    global nextID
+    nextID += 1
+    chatbot = ChatBot("SearchGPT",
+                      botType="Web UI based text-based search assist chatbot. The source code is available on https://github.com/perrys25/SearchGPT",
+                      progress=lambda p: server.send(client, json.dumps(p)), commands=[search])
+    chatbots[client] = chatbot
 
-asyncio.get_event_loop().run_forever()
+
+def on_error(exception):
+    """Called by the WebSocket server whenever an Exception is thrown."""
+    # Your implementation here.
+
+
+def on_connection_close(client):
+    """Called by the WebSocket server when a connection is closed."""
+    # Your implementation here.
+
+
+def on_server_destruct():
+    """Called immediately prior to the WebSocket server shutting down."""
+    # Your implementation here.
+
+server = WebSocketServer(
+    "0.0.0.0",
+    8080,
+    on_data_receive=on_data_receive,
+    on_connection_open=on_connection_open,
+    on_error=on_error,
+    on_connection_close=on_connection_close,
+    on_server_destruct=on_server_destruct
+)
+
+server.serve_forever()
